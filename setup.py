fe321e3f5e50b73143968aa6023c831ee231dd00
Giel van Schijndel <me@mortis.eu>
2008-11-22 16:21:58 +0100

    Add some package info to the egg's data

--- setup.py
+++ setup.py
@@ -7,6 +7,16 @@ VERSION = '0.1'
 setup(
     name=PACKAGE,
     version=VERSION,
+    author='Giel van Schijndel',
+    author_email='me@mortis.eu',
+    url='http://trac-hacks.org/wiki/CodeTagsPlugin',
+    download_url='http://git.mortis.eu/codetags.git',
+    license='BSD',
+    classifiers=[
+        'Framework :: Trac',
+        'License :: OSI Approved :: BSD License',
+    ],
+    keywords='trac plugin',
     packages=['codetags'],
     package_data={
         'codetags' : ['templates/*.cs', 'htdocs/*.css']
