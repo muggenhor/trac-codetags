074335cbfbb2eb3123c94a07a6c6919eec05a4d3
Giel van Schijndel <giel@zaak.blibblib.nl>
2010-05-21 20:45:43 +0200

    Include the commit date in the version number for non-tagged commits
    Signed-off-by: Giel van Schijndel <me@mortis.eu>

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 from setuptools import setup
+import os
 
 PACKAGE = 'codetags'
 VERSION = '0.2'
+
+if os.path.exists('.git/HEAD'):
+    tag_hash = os.popen('git log --pretty=format:%%H %s^..%s' % (VERSION, VERSION)).read()
+    head_hash = os.popen('git log --pretty=format:%H HEAD^..HEAD').read()
+    if tag_hash != head_hash:
+        import datetime
+        date = os.popen('git log --pretty=format:%ct HEAD^..HEAD').read()
+        date = datetime.datetime.utcfromtimestamp(long(date))
+        VERSION += date.strftime('-%Y%m%d%H%M%S')
 
 setup(
     name=PACKAGE,
