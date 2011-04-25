66b43879a84021de84f0a20242e75eaacfb5ad37
Giel van Schijndel <me@mortis.eu>
2011-04-25 15:46:57 +0200

    Wrap git version-date extraction into a somewhat reusable function
    Signed-off-by: Giel van Schijndel <me@mortis.eu>

--- setup.py
+++ setup.py
@@ -2,21 +2,24 @@
 from setuptools import setup
 import os
 
-PACKAGE = 'codetags'
-VERSION = '0.3'
-
-if os.path.exists('.git/HEAD'):
-    tag_hash = os.popen('git rev-list --max-count=1 %s .' % (VERSION,)).read()
-    head_hash = os.popen('git rev-list --max-count=1 HEAD .').read()
-    if tag_hash != head_hash:
-        import datetime
-        date = os.popen('git log --pretty=format:%%ct --max-count=1 %s' % (head_hash,)).read()
-        date = datetime.datetime.utcfromtimestamp(long(date))
-        VERSION += date.strftime('-%Y%m%d%H%M%S')
+def version_from_tag(tag, prefix='v'):
+    gitdir = dir = os.path.dirname(os.path.abspath(__file__))
+    while gitdir != '/':
+        if os.path.exists(os.path.join(gitdir, '.git/HEAD')):
+            tag_hash = os.popen('git rev-list --max-count=1 %s %s' % (prefix + tag, dir)).read()
+            head_hash = os.popen('git rev-list --max-count=1 HEAD %s' % (dir,)).read()
+            if tag_hash != head_hash:
+                import datetime
+                date = os.popen('git log --pretty=format:%%ct --max-count=1 %s' % (head_hash,)).read()
+                date = datetime.datetime.utcfromtimestamp(long(date))
+                tag += date.strftime('-%Y%m%d%H%M%S')
+            break
+        gitdir = os.path.dirname(gitdir)
+    return tag
 
 setup(
-    name=PACKAGE,
-    version=VERSION,
+    name='codetags',
+    version=version_from_tag('0.3', prefix=''),
     install_requires='Trac >= 0.10',
     author='Giel van Schijndel',
     author_email='me@mortis.eu',
